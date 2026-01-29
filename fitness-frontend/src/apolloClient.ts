bd55f82d3ea513f51052ad11122055d2aac98ad0
Zay <destinygamingblogs@gmail.com>
2026-01-28 23:04:35 -0500

    #### Rails configuration

--- fitness-frontend/src/apolloClient.ts
+++ fitness-frontend/src/apolloClient.ts
@@ -1,8 +1,9 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
 
 const client = new ApolloClient({
-    link: new HttpLink({ uri: "http://localhost:3000/graphql" }),
+    link: new HttpLink({ uri: `${API_URL}/graphql` }),
     cache: new InMemoryCache(),
 });
 
